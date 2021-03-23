38586db29c9c68caaad3113e7f51e68fa241264e
HitLing <31694201+HitLing@users.noreply.github.com>
2021-03-23 11:31:58 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+
+public class ASum
+{
+
+    static  long cube(long a) {
+        return a * a * a;
+    }
+    static long counterofcubes(long m) {
+        long count;
+        count = 0;
+        long n = 1;
+        while (m > 0) {
+            m = m - cube(n);
+            n++;
+            count++;
+        }
+        if (m == 0)
+        {
+            return count;
+        }
+        else {
+            return -1;
+        }
+    }
+
+
+
+    public static long findNb(long m)
+    {
+        long countofcubes;
+        countofcubes = counterofcubes(m);
+        return countofcubes;
+    }
+
+}
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert.ToString(2/10));
+        }
+    }
+}
